8495fb512ae31290bcee0faa4aa17e1d293b5bb1
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-03-18 15:30:00 +0800

    Implement manual lowercase check without built-in function

--- batch_number7/p4_check_lowercase_without_islower.py
+++ batch_number7/p4_check_lowercase_without_islower.py
@@ -1,3 +1,14 @@
 class Text:
     def __init__(self, text):
         self.text = text
+
+    def is_lowercase(self):
+        has_letter = False
+
+        for character in self.text:
+            if 'A' <= character <= 'Z':
+                return False
+            elif 'a' <= character <= 'z':
+                has_letter = True
+
+        return has_letter
