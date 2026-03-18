bdfde89a0a3db8026150fbe5dbdaa63e6f9d8d97
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-03-18 15:30:00 +0800

    Implement manual right justification without built-in function

--- batch_number7/p6_rjust_without_rjust.py
+++ batch_number7/p6_rjust_without_rjust.py
@@ -1,3 +1,11 @@
 class Text:
     def __init__(self, text):
         self.text = text
+
+    def right_justify(self, width):
+        result = self.text
+
+        while len(result) < width:
+            result = " " + result
+
+        return result
