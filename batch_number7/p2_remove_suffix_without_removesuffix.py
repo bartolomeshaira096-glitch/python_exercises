da3c72199133e49eaa0b536685d3d121d558ed29
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-03-18 15:30:00 +0800

    Implement manual removesuffix functionality

--- batch_number7/p2_remove_suffix_without_removesuffix.py
+++ batch_number7/p2_remove_suffix_without_removesuffix.py
@@ -1,3 +1,12 @@
 class Text:
     def __init__(self, text):
         self.text = text
+
+    def remove_suffix(self, suffix):
+        if len(suffix) > len(self.text):
+            return self.text
+
+        if self.text[-len(suffix):] == suffix:
+            return self.text[:-len(suffix)]
+
+        return self.text
