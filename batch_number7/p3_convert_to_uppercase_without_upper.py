b1342c4a1873dd411509b5377eb827916a973724
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-03-18 15:30:00 +0800

    Implement manual uppercase conversion without built-in function

--- batch_number7/p3_convert_to_uppercase_without_upper.py
+++ batch_number7/p3_convert_to_uppercase_without_upper.py
@@ -1,3 +1,14 @@
 class Text:
     def __init__(self, text):
         self.text = text
+
+    def to_uppercase(self):
+        result = ""
+
+        for character in self.text:
+            if 'a' <= character <= 'z':
+                result += chr(ord(character) - 32)
+            else:
+                result += character
+
+        return result
