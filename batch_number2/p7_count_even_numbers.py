4ef6041834e91777f04f2241332e51a51b7cb2b4
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-03-18 15:30:00 +0800

    Add logic for counting even numbers
    Add output for total even numbers

--- batch_number2/p7_count_even_numbers.py
+++ batch_number2/p7_count_even_numbers.py
@@ -2,3 +2,8 @@ even_count = 0
 
 for i in range(10):
     number = int(input(f"Enter number {i + 1}: "))
+
+    if number % 2 == 0:
+        even_count += 1
+
+print("Number of even numbers:", even_count)
