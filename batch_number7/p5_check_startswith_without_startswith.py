2970d792a9d77789f11512b15e4f317c6cbce648
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-03-18 15:30:00 +0800

    Implement manual startswith functionality

--- batch_number7/p5_check_startswith_without_startswith.py
+++ batch_number7/p5_check_startswith_without_startswith.py
@@ -1,3 +1,9 @@
 class Text:
     def __init__(self, text):
         self.text = text
+
+    def starts_with(self, prefix):
+        if len(prefix) > len(self.text):
+            return False
+
+        return self.text[:len(prefix)] == prefix
