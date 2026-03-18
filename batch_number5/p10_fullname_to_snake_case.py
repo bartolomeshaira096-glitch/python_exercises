eecf50c08e103c62b0448a8514af23745b3f44c9
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-03-18 15:30:00 +0800

    Add input and display snake case fullname

--- batch_number5/p10_fullname_to_snake_case.py
+++ batch_number5/p10_fullname_to_snake_case.py
@@ -4,3 +4,9 @@ class FullName:
 
     def to_snake_case(self):
         return "_".join(self.fullname.lower().split())
+
+fullname = input("Enter your full name: ")
+
+person = FullName(fullname)
+
+print(person.to_snake_case())
