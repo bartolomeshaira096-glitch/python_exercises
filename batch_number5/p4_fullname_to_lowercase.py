891fd234ec04015e4df2ae52a16184a560bd3b95
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-03-18 15:30:00 +0800

    Add input and display lowercase fullname

--- batch_number5/p4_fullname_to_lowercase.py
+++ batch_number5/p4_fullname_to_lowercase.py
@@ -4,3 +4,9 @@ class FullName:
 
     def to_lowercase(self):
         return self.fullname.lower()
+    
+fullname = input("Enter your full name: ")
+
+person = FullName(fullname)
+
+print(person.to_lowercase())
