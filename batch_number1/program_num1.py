ac7c729a273d4ccb2e1de8bedf1e592e84578866
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-03-18 15:30:00 +0800

    determine the bigger number using if else statement then print the bigger number

--- batch_number1/program_num1.py
+++ batch_number1/program_num1.py
@@ -1,3 +1,8 @@
-# INPUT NUM PRINT BIGGER NUMBER
+# ASKING A NUMBER
 first_number = float(input("enter first number: "))
 second_number = float(input("enter second number: "))
+# DETERMINE THE BIGGER NUMBER
+if first_number > second_number:
+    print(f"the bigger number is:", first_number)
+else:
+    print(f"the bigger number is:", second_number)
