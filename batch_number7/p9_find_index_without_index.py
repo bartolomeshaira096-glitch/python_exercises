89b2b825a19666555b77a6d71877a78aad1674c7
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-03-18 15:30:00 +0800

    Implement manual index search without built-in index function

--- batch_number7/p9_find_index_without_index.py
+++ batch_number7/p9_find_index_without_index.py
@@ -1,3 +1,10 @@
 class Text:
     def __init__(self, text):
         self.text = text
+
+    def custom_index(self, character):
+        for position in range(len(self.text)):
+            if self.text[position] == character:
+                return position
+
+        return -1
