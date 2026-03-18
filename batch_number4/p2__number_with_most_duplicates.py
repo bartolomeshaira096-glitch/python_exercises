1c3f69810dec4d0e700afb4ad737fa7cdc7a9c13
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-03-18 15:30:00 +0800

    Add logic to find most frequently occurring number

--- batch_number4/p2__number_with_most_duplicates.py
+++ batch_number4/p2__number_with_most_duplicates.py
@@ -6,3 +6,12 @@ while True:
         numbers.append(num)
     except ValueError:
         break
+
+most_number = None
+highest_count = 0
+
+for num in numbers:
+    count = numbers.count(num)
+    if count > highest_count:
+        highest_count = count
+        most_number = num
