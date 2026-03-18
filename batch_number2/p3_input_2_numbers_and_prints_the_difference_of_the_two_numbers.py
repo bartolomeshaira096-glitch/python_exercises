0e3e8589013f07ab67026b20177e34072eb16f29
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-03-18 15:30:00 +0800

    Add difference calculation and output for difference of two numbers

--- batch_number2/p3_input_2_numbers_and_prints_the_difference_of_the_two_numbers.py
+++ batch_number2/p3_input_2_numbers_and_prints_the_difference_of_the_two_numbers.py
@@ -1,2 +1,6 @@
 first_number = float(input("Enter first number: "))
 second_number = float(input("Enter second number: "))
+
+difference = first_number - second_number
+
+print("Difference:", difference)
