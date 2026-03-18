0a550b8caf30e37d9bc3b3b718faf6bc3be9f255
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-03-18 15:30:00 +0800

    Implement manual rstrip functionality without built-in function

--- batch_number7/p1_remove_trailing_spaces_without_rstrip.py
+++ batch_number7/p1_remove_trailing_spaces_without_rstrip.py
@@ -1,3 +1,11 @@
 class Text:
     def __init__(self, text):
         self.text = text
+
+    def remove_trailing_spaces(self):
+        index = len(self.text) - 1
+
+        while index >= 0 and self.text[index] == " ":
+            index -= 1
+
+        return self.text[:index + 1]
