c5915772680cbd398cd1beac50a0ee51a87e3bf0
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-03-18 15:30:00 +0800

    Add method to convert fullname to pascal case

--- batch_number5/p9_fullname_to_pascal_case.py
+++ batch_number5/p9_fullname_to_pascal_case.py
@@ -1,3 +1,7 @@
 class FullName:
     def __init__(self, fullname):
         self.fullname = fullname
+
+    def to_pascal_case(self):
+        words = self.fullname.split()
+        return ''.join(word.capitalize() for word in words)
