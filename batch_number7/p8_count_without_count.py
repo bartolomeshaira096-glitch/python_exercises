1cad3f120ab8d9b14ccb15738f16f8f1938c0124
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-03-18 15:30:00 +0800

    Implement manual character counting without built-in count function

--- batch_number7/p8_count_without_count.py
+++ batch_number7/p8_count_without_count.py
@@ -1,3 +1,12 @@
 class Text:
     def __init__(self, text):
         self.text = text
+
+    def custom_count(self, character):
+        counter = 0
+
+        for ch in self.text:
+            if ch == character:
+                counter += 1
+
+        return counter
