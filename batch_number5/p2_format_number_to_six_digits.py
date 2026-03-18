6b1d68dcac5fa992490222ae2ebcad6bb3d31455
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-03-18 15:30:00 +0800

    Add input and display formatted number

--- batch_number5/p2_format_number_to_six_digits.py
+++ batch_number5/p2_format_number_to_six_digits.py
@@ -5,4 +5,7 @@ class NumberFormatter:
     def to_six_digits(self):
         return f"{self.number:06d}"
     
-    number = int(input("Enter a number (0-1000): "))
+number = int(input("Enter a number (0-1000): "))
+
+formatter = NumberFormatter(number)
+print(formatter.to_six_digits())
