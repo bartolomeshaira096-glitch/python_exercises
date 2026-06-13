b4aa60ebdf15ad89df99b940315d02d3bf9ed2ed
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-06-13 22:12:43 +0800

    Added a text in self.text in Class Text and Implement manual reverse index search without built-in rindex function

--- batch_number7/p10_rindex_without_rindex.py
+++ batch_number7/p10_rindex_without_rindex.py
@@ -1,3 +1,10 @@
 class Text:
     def __init__(self, text):
         self.text = text
+        
+    def custom_rindex(self, character):
+        for position in range(len(self.text) - 1, -1, -1):
+            if self.text[position] == character:
+                return position
+
+        return -1
