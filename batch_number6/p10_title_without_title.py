aabeb5d23fdb62ff1613812e2262424f2c218288
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-06-13 21:33:35 +0800

    Add custom title case method

--- batch_number6/p10_title_without_title.py
+++ batch_number6/p10_title_without_title.py
@@ -1,3 +1,26 @@
 class Text:
     def __init__(self, text):
         self.text = text
+
+    def to_title_case(self):
+        result = ""
+        new_word = True
+
+        for character in self.text:
+            if character == " ":
+                result += character
+                new_word = True
+            else:
+                if new_word:
+                    if 'a' <= character <= 'z':
+                        result += chr(ord(character) - 32)
+                    else:
+                        result += character
+                    new_word = False
+                else:
+                    if 'A' <= character <= 'Z':
+                        result += chr(ord(character) + 32)
+                    else:
+                        result += character
+
+        return result
