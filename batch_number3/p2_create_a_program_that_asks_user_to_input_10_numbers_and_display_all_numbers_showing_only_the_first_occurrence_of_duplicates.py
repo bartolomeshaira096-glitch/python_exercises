a76b13fdb527e504f6b68e01464f5e977f6c1341
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-06-13 16:48:19 +0800

    Add logic to display only first occurrence of duplicate numbers

--- batch_number3/p2_create_a_program_that_asks_user_to_input_10_numbers_and_display_all_numbers_showing_only_the_first_occurrence_of_duplicates.py
+++ batch_number3/p2_create_a_program_that_asks_user_to_input_10_numbers_and_display_all_numbers_showing_only_the_first_occurrence_of_duplicates.py
@@ -3,3 +3,10 @@ numbers = []
 for i in range(10):
     number = int(input(f"Enter number {i + 1}: "))
     numbers.append(number)
+
+displayed = []
+
+for number in numbers:
+    if number not in displayed:
+        print(number)
+        displayed.append(number)
