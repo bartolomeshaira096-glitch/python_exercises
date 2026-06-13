de10aaa7ac312235c3c3cd4185b1dcb6885182e3
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-06-13 21:04:47 +0800

    Add custom center alignment method

--- batch_number6/p7_center_without_center.py
+++ batch_number6/p7_center_without_center.py
@@ -1,3 +1,14 @@
 class Text:
     def __init__(self, text):
         self.text = text
+    
+    def center_text(self, width):
+        total_spaces = width - len(self.text)
+
+        if total_spaces <= 0:
+            return self.text
+
+        left_spaces = total_spaces // 2
+        right_spaces = total_spaces - left_spaces
+
+        return (" " * left_spaces) + self.text + (" " * right_spaces)
