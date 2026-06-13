a68d5ea4db0b12fd1cbbf4cf657c4adb3cdfd7b6
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-06-13 19:38:30 +0800

    Add custom leading space removal method

--- batch_number6/p1_remove_leading_spaces_without_lstrip.py
+++ batch_number6/p1_remove_leading_spaces_without_lstrip.py
@@ -1,3 +1,11 @@
 class Text:
     def __init__(self, text):
         self.text = text
+    
+    def remove_leading_spaces(self):
+        index = 0
+
+        while index < len(self.text) and self.text[index] == " ":
+            index += 1
+
+        return self.text[index:]
