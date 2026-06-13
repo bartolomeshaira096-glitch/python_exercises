07ef99c3fccaf17fdf4874c02779dd028147c44d
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-06-13 20:56:14 +0800

    Add custom left justify method

--- batch_number6/p6_ljust_without_ljust.py
+++ batch_number6/p6_ljust_without_ljust.py
@@ -2,3 +2,10 @@ class Text:
     def __init__(self, text):
         self.text = text
         
+    def left_justify(self, width):
+        result = self.text
+
+        while len(result) < width:
+            result += " "
+
+        return result
