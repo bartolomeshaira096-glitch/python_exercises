8903f6dc7db84955fec7a15ef5a01bd41d722a84
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-06-13 20:19:42 +0800

    Add custom lowercase conversion method

--- batch_number6/p3_convert_to_lowercase_without_lower.py
+++ batch_number6/p3_convert_to_lowercase_without_lower.py
@@ -1,3 +1,14 @@
 class Text:
     def __init__(self, text):
-        self.text = text
+        self.text = text 
+
+    def to_lowercase(self):
+        result = ""
+
+        for char in self.text:
+            if 'A' <= char <= 'Z':
+                result += chr(ord(char) + 32)
+            else:
+                result += char
+
+        return result
