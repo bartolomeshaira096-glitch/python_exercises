ac8b235bd40f6436118ace3285d5a41de8f5a243
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-06-13 17:05:25 +0800

    Add logic to detect unique and duplicate numbers

--- batch_number3/p3_unique_duplicate_checker.py
+++ batch_number3/p3_unique_duplicate_checker.py
@@ -3,4 +3,9 @@ numbers = []
 while True:
     try:
         num = int(input("Enter a number: "))
-        
+        numbers.append(num)
+
+        if numbers.count(num) == 1:
+            print("Unique")
+        else:
+            print("Duplicate")
