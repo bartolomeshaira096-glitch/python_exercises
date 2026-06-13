d7c3235caea2e8a80d4a1cad499f055125dc1ea7
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-06-13 20:44:18 +0800

    Add custom endswith checking method

--- batch_number6/p5_check_endswith_without_endswith.py
+++ batch_number6/p5_check_endswith_without_endswith.py
@@ -1,3 +1,9 @@
 class Text:
     def __init__(self, text):
         self.text = text
+
+    def ends_with(self, suffix):
+        if len(suffix) > len(self.text):
+            return False
+
+        return self.text[-len(suffix):] == suffix
