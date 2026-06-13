c17b8f49ef0f60dd1dda050ad743bca5b95eed64
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-06-13 19:51:51 +0800

    Add custom remove prefix method

--- batch_number6/p2_remove_prefix_without_removeprefix.py
+++ batch_number6/p2_remove_prefix_without_removeprefix.py
@@ -1,3 +1,9 @@
 class Text:
     def __init__(self, text):
         self.text = text
+
+    def remove_prefix(self, prefix):
+        if self.text[:len(prefix)] == prefix:
+            return self.text[len(prefix):]
+
+        return self.text
