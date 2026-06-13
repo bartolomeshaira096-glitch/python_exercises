8ad02d15b82c81135bf55407346d7a0741bbe0b7
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-06-13 20:30:33 +0800

    Add custom uppercase checking method

--- batch_number6/p4_check_uppercase_without_isupper.py
+++ batch_number6/p4_check_uppercase_without_isupper.py
@@ -1,3 +1,14 @@
 class Text:
     def __init__(self, text):
-        self.text = text
+        self.text = 
+        
+    def is_uppercase(self):
+        has_letter = False
+
+        for char in self.text:
+            if 'a' <= char <= 'z':
+                return False
+            elif 'A' <= char <= 'Z':
+                has_letter = True
+
+        return has_letter
