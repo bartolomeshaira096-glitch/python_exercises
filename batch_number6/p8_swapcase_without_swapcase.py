cbcf6b19fcbd354da539c79adf4f819f286227c3
unknown <bartolomeshaira096@gmail.com$ git config --global user.email bartolomeshaira096@gmail.com>
2026-06-13 21:15:50 +0800

    Add input and display reverse casing

--- batch_number6/p8_swapcase_without_swapcase.py
+++ batch_number6/p8_swapcase_without_swapcase.py
@@ -1,3 +1,22 @@
 class Text:
     def __init__(self, text):
         self.text = text
+
+    def reverse_case(self):
+        result = ""
+
+        for char in self.text:
+            if 'A' <= char <= 'Z':
+                result += chr(ord(char) + 32)
+            elif 'a' <= char <= 'z':
+                result += chr(ord(char) - 32)
+            else:
+                result += char
+
+        return result
+    
+text = input("Enter a string: ")
+
+value = Text(text)
+
+print(value.reverse_case())
